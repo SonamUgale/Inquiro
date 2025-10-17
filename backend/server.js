862a38544c6b596ae0ebac04a6c03332ca024c9c
Sonam Ugale <sonamugale25@gmail.com>
2025-10-17 21:50:56 +0530

    changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,27 @@ import { configDotenv } from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
 import chatRoute from "./routes/chat.js";
+import path from "path";
+import { fileURLToPath } from "url";
 configDotenv();
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/api", chatRoute);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+  });
+}
 const connectDB = async () => {
   try {
     console.log("Mongo URI:", process.env.MONGO_URI);
